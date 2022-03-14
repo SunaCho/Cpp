e91fc331c0d10c7094e79f8c5d668ea90a7b86ae
SunaCho <80630143+SunaCho@users.noreply.github.com>
2022-03-15 00:40:03 +0900

    Add files via upload

---
+++ 2022-1/Number Theory/2740.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int N,M,K;
+int A[101][101],B[100][100];
+int res[100][100];
+
+int main() {
+    cin>>N>>M;
+
+    for(int i=0; i<N; i++)
+        for(int j=0; j<M; j++)  cin>>A[i][j];
+
+    cin>>M>>K;
+
+    for(int i=0; i<M; i++)
+        for(int j=0; j<K; j++)  cin>>B[i][j];
+
+    for(int i=0; i<N; i++)
+        for(int j=0; j<K; j++)
+            for(int k=0; k<M; k++)  res[i][j]+=A[i][k]*B[k][j];
+    
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<K; j++) {
+            cout<<res[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
